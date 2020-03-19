4368c752ac59820438b14bab016c9ac30856f787
Yash Karandikar <nerdstep710@gmail.com>
2020-03-19 13:44:44 -0500

    Add README and license to setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,24 @@
 from setuptools import setup
 import alliancepy
 
+with open("README.rst") as f:
+    readme = f.read()
+
 setup(
     name="alliancepy",
     version=alliancepy.__version__,
     packages=["alliancepy"],
+    url="https://github.com/karx1/alliancepy",
+    license="MIT",
     author="karx",
     author_email="nerdstep710@gmail.com",
     description="A library to access The Orange Alliance API",
-    python_requires=">=3.6"
+    long_description=readme,
+    python_requires=">=3.6",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+    ]
 )
