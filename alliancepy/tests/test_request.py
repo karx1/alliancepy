5bbe482a609f6f82b115316d8704e4ffdc4a6314
Yash Karandikar <nerdstep710@gmail.com>
2020-03-23 15:59:57 -0500

    Run black formatter

--- alliancepy/tests/test_request.py
+++ alliancepy/tests/test_request.py
@@ -6,9 +6,11 @@ import alliancepy.team
 def create_team(team_number: int):
     headers = {
         "api-key": "1e48fa3b34a8ab86cbec44735c5b6055a141f245455faac878bfa204e35c1a7e",
-        "application-name": "alliancepy-test"
+        "application-name": "alliancepy-test",
     }
-    client = alliancepy.Client(api_key=headers["api-key"], application_name=headers["application-name"])
+    client = alliancepy.Client(
+        api_key=headers["api-key"], application_name=headers["application-name"]
+    )
     team = client.team(team_number)
     return team
 
@@ -21,7 +23,7 @@ class TestRequest(unittest.TestCase):
     def test_response(self):
         team = create_team(16405)
         self.assertEqual(team.rookie_year, 2019)
-    
+
     def test_interaction(self):
         team1 = create_team(16405)
         team2 = create_team(16405)
